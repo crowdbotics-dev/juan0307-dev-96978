3b0bac527343e37f1030c870c0158c0070030311
crowdbotics-dev <engineering@crowdbotics.com>
2023-07-03 14:55:26 +0000

    Update models

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Malzhar
+admin.site.register(Malzhar)
 
 # Register your models here.
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,5 @@
+from django.conf import settings
 from django.db import models
+class Malzhar(models.Model):
+    'Generated Model'
+    armor = models.BigIntegerField()
